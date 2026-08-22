697de6b47d762011f9038b62f78aef78aa83a467
beejh02 <joon0448@naver.com>
2026-08-23 00:22:02 +0900

    feat: springboot - docker DB 연결

---
+++ backend/nurigo/src/main/java/com/nurigo/nurigo/NurigoApplication.java
@@ -0,0 +1,13 @@
+package com.nurigo.nurigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurigoApplication.class, args);
+	}
+
+}
---
+++ backend/nurigo/src/test/java/com/nurigo/nurigo/NurigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurigo.nurigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
