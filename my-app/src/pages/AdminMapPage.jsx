b516d9c4ed6a17b18e9d2d74e81a0724c1304bf6
beejh02 <joon0448@naver.com>
2026-08-22 14:57:45 +0900

    feat: 관리자뷰에 Polygon 관련 기능 붙여넣기

--- my-app/src/pages/AdminMapPage.jsx
+++ my-app/src/pages/AdminMapPage.jsx
@@ -3,18 +3,37 @@ import { useState } from "react";
 import NaverMap from "../components/NaverMap";
 import MapSearch from "../components/MapSearch";
 import CurrentLocation from "../components/CurrentLocation";
+import MarketPolygon from "../components/MarketPolygon";
+import PolygonEditor from "../components/PolygonEditor";
 
 import "../App.css";
 
 function AdminMapPage() {
   const [map, setMap] = useState(null);
 
+  /*
+   * PolygonEditor와 MarketPolygon이
+   * 공유하는 시장 영역 좌표.
+   *
+   * PolygonEditor:
+   * 좌표 생성 / 수정
+   *
+   * MarketPolygon:
+   * 좌표를 지도에 표시
+   */
+  const [marketBoundary, setMarketBoundary] = useState([]);
+
   return (
     <div className="app">
       <NaverMap onMapReady={setMap} />
+      <MarketPolygon map={map} coordinates={marketBoundary} />
+      <PolygonEditor
+        map={map}
+        coordinates={marketBoundary}
+        onChange={setMarketBoundary}
+      />
       <MapSearch map={map} />
       <CurrentLocation map={map} />
-      {/* PolygonEditor는 나중에 추가 */}
     </div>
   );
 }
