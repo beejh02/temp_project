7930c418c7cbb9b2cd380ca8ac9f20ec2882b4de
beejh02 <joon0448@naver.com>
2026-08-22 13:55:08 +0900

    feat: 관리자 및 사용자 뷰 현재 내 위치 컴포넌트 포함 기능 추가

--- my-app/src/pages/AdminMapPage.jsx
+++ my-app/src/pages/AdminMapPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import NaverMap from "../components/NaverMap";
 import MapSearch from "../components/MapSearch";
+import CurrentLocation from "../components/CurrentLocation";
 
 import "../App.css";
 
@@ -11,9 +12,8 @@ function AdminMapPage() {
   return (
     <div className="app">
       <NaverMap onMapReady={setMap} />
-
       <MapSearch map={map} />
-
+      <CurrentLocation map={map} />
       {/* PolygonEditor는 나중에 추가 */}
     </div>
   );
