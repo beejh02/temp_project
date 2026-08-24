7b6560647cf451d20f251ae7169c2eff6b90f395
beejh02 <joon0448@naver.com>
2026-08-24 22:34:24 +0900

    feat: 실제 market polygon 생성 기능 추가

--- backend/nurigo/src/main/java/com/nurigo/nurigo/market/service/MarketService.java
+++ backend/nurigo/src/main/java/com/nurigo/nurigo/market/service/MarketService.java
@@ -2,20 +2,37 @@ package com.nurigo.nurigo.market.service;
 
 import java.util.List;
 
+import org.locationtech.jts.geom.Polygon;
 import org.springframework.stereotype.Service;
 
+import com.nurigo.nurigo.market.dto.MarketCreateRequest;
 import com.nurigo.nurigo.market.entity.Market;
+import com.nurigo.nurigo.market.mapper.MarketGeometryMapper;
 import com.nurigo.nurigo.market.repository.MarketRepository;
 
 @Service
 public class MarketService {
-    private final MarketRepository marketRepository;
 
-    public MarketService(MarketRepository marketRepository) {
+    private final MarketRepository marketRepository;
+    private final MarketGeometryMapper marketGeometryMapper;
+
+    public MarketService(
+            MarketRepository marketRepository,
+            MarketGeometryMapper marketGeometryMapper
+    ) {
         this.marketRepository = marketRepository;
+        this.marketGeometryMapper = marketGeometryMapper;
     }
 
-    public Market save(Market market) {
+    public Market create(MarketCreateRequest request) {
+        Polygon boundary
+                = marketGeometryMapper.toPolygon(request.boundary());
+
+        Market market = new Market(
+                request.name(),
+                boundary
+        );
+
         return marketRepository.save(market);
     }
 
