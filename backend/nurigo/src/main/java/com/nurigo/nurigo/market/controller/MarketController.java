85b16ca3b2efa1b5d53ea9499f7f7ee9b3396eb5
beejh02 <joon0448@naver.com>
2026-08-25 23:35:33 +0900

    feat: 시장 전체 조회 API 추가

--- backend/nurigo/src/main/java/com/nurigo/nurigo/market/controller/MarketController.java
+++ backend/nurigo/src/main/java/com/nurigo/nurigo/market/controller/MarketController.java
@@ -1,13 +1,17 @@
 package com.nurigo.nurigo.market.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nurigo.nurigo.market.dto.MarketCreateRequest;
+import com.nurigo.nurigo.market.dto.MarketResponse;
 import com.nurigo.nurigo.market.entity.Market;
 import com.nurigo.nurigo.market.service.MarketService;
 
@@ -32,5 +36,12 @@ public class MarketController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(createdMarket.getId());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MarketResponse>> getMarkets() {
+        return ResponseEntity.ok(
+                marketService.findAll()
+        );
     }
 }
